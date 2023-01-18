f10837e8fea447d76eb7c35a408e28b42e6719a7
Zygimantass <zygimantelis@gmail.com>
2023-01-18 12:43:44 -0500

    fix: change allowlist logic to make it work

--- pathfilter.go
+++ pathfilter.go
@@ -68,25 +68,30 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 }
 
 func (pf *PathFilter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	currentPath := req.URL.EscapedPath()
+        currentPath := req.URL.EscapedPath()
 
-	if len(pf.blocklist) > 0 {
-		for _, re := range pf.blocklist {
-			if re.MatchString(currentPath) {
-				http.Error(rw, "This path is blocked", http.StatusForbidden)
-				return
-			}
-		}
-	}
+        os.Stdout.WriteString(fmt.Sprintf("bl: %s\n", pf.allowlist))
+        if len(pf.blocklist) > 0 {
+                for _, re := range pf.blocklist {
+                        if re.MatchString(currentPath) {
+                                os.Stdout.WriteString(fmt.Sprintf("blocking %s for %s", currentPath, re))
+                                http.Error(rw, "This path is blocked", http.StatusForbidden)
+                                return
+                        }
+                }
+        }
 
-	if len(pf.allowlist) > 0 {
-		for _, re := range pf.allowlist {
-			if !re.MatchString(currentPath) {
-				http.Error(rw, "This path is blocked", http.StatusForbidden)
-				return
-			}
-		}
-	}
+        if len(pf.allowlist) > 0 {
+                for _, re := range pf.allowlist {
+                        if re.MatchString(currentPath) {
+                                pf.next.ServeHTTP(rw, req)
+                                os.Stdout.WriteString(fmt.Sprintf("blocking %s for %s", currentPath, re))
+                                return
+                        }
+                }
+                http.Error(rw, "This path is blocked", http.StatusForbidden)
+                return
+        }
 
-	pf.next.ServeHTTP(rw, req)
+        pf.next.ServeHTTP(rw, req)
 }
